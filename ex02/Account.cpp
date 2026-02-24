f449bf66d4dfdf2c4be09ed653fbbe26e77a269e
benes-al <gryk_@hotmail.com>
2026-02-24 16:14:31 +0000

    refactor: clean up Account class and remove unnecessary files

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -17,7 +17,7 @@ Account::Account(int initial_deposit) : _amount(initial_deposit) {
     
     _displayTimestamp();
     
-    std::cout   << "index:"  << _accountIndex 
+    std::cout   << "index:" << _accountIndex 
     << ";amount:" << initial_deposit 
     << ";created" << std::endl;
 }
@@ -30,60 +30,114 @@ void    Account::_displayTimestamp( void ) {
 
     char buffer[20];
     
-    std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", localTime); 
+    std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", localTime);
     
     std::cout << buffer; 
 }
 
 void Account::displayAccountsInfos( void ) {
+    _displayTimestamp();
+
     std::cout   << "accounts:" << _nbAccounts 
                 << ";total:" << _totalAmount 
                 << ";deposits:" << _totalNbDeposits 
                 << ";withdrawals:" << _totalNbWithdrawals << std::endl;        
 }
 
+//Methods
+
+void Account::displayStatus(void) const { 
+    _displayTimestamp();
+    
+    std::cout   << "index:" << _accountIndex
+                << ";amount:" << _amount
+                << ";deposits:" << _nbDeposits
+                << ";withdrawals:" << _nbWithdrawals
+                << std::endl;
+}
+
 // Setters
 
 void Account::makeDeposit(int deposit) {
-    _totalAmount += deposit;
-    
+    _displayTimestamp();
 
+    int p_amount = _amount;
+    _amount += deposit;
+    _nbDeposits++;
+    
+    std::cout   << "index:" << _accountIndex
+                << ";p_amount:" << p_amount
+                << ";deposit:" << deposit
+                << ";amount:" << _amount
+                << ";nb_deposits:" << _nbDeposits
+                << std::endl; 
+
+    _totalAmount += deposit;
+    _totalNbDeposits++;
 }
 
 bool Account::makeWithdrawal(int withdrawal) {
+    bool approved = true;
+    int amount = _amount - withdrawal;
+
+    if (amount < 0){
+        approved = false;
+    }
+
+    _displayTimestamp();
     
+    std::cout   << "index:" << _accountIndex
+                << ";p_amount:" << _amount;
+
+    if (!approved){
+        std::cout   << ";withdrawal:refused"
+                    << std::endl;
+        return approved;
+    }
+
+    _amount -= withdrawal;
+    _nbWithdrawals++;
+    _totalAmount -= withdrawal;
+    _totalNbWithdrawals++;
+    
+    std::cout   << ";withdrawal:" << withdrawal
+                << ";amount:" << _amount
+                << ";nb_withdrawals:" << _nbWithdrawals
+                << std::endl; 
+    return approved;
 }
 
 int  Account::checkAmount(void) const {
-    
+    return _amount ;
 }
 
 //Getters
 
-void Account::displayStatus(void) const { 
-
-}
-
 int Account::getNbAccounts( void ) {
-    return ( _nbAccounts );
+    return _nbAccounts;
 }
 
 int Account::getTotalAmount( void ) {
-    return( _totalAmount );
+    return _totalAmount;
 }
 
 int Account::getNbDeposits( void ) {
-    return( _totalNbDeposits );
+    return _totalNbDeposits;
 }
 
 int	Account::getNbWithdrawals( void ) {
-    return ( _totalNbWithdrawals );
+    return _totalNbWithdrawals;
 }
 
 
 // Destructor
 Account::~Account(void) {
+    _displayTimestamp();
     
+    std::cout   << "index:" << _accountIndex 
+                << ";amount:" << _amount 
+                << ";closed" << std::endl;
+    
+    _nbAccounts--;
+    _totalAmount -= _amount;
 }
-
-
