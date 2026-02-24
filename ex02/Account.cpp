8a5787dadd75068e65dab4daeb7cbf152ce4b085
benes-al <gryk_@hotmail.com>
2026-02-24 13:30:33 +0000

    feat: implement Account class constructor and display methods

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,12 +1,69 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-//getters
+// Constructor
+
+Account::Account(int initial_deposit) : _amount(initial_deposit) {
+    _accountIndex = _nbAccounts++;
+    _totalAmount += _amount;
+    _nbDeposits = 0;
+    _nbWithdrawals = 0;
+    
+    _displayTimestamp();
+    
+    std::cout   << "index:"  << _accountIndex 
+    << ";amount:" << initial_deposit 
+    << ";created" << std::endl;
+}
+
+// Static Method
+
+void    Account::_displayTimestamp( void ) {
+    std::time_t now = std::time(0);
+    std::tm* localTime = std::localtime(&now);
+
+    char buffer[20];
+    
+    std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", localTime); 
+    
+    std::cout << buffer; 
+}
+
+void Account::displayAccountsInfos( void ) {
+    std::cout   << "accounts:" << _nbAccounts 
+                << ";total:" << _totalAmount 
+                << ";deposits:" << _totalNbDeposits 
+                << ";withdrawals:" << _totalNbWithdrawals << std::endl;        
+}
+
+// Setters
+
+void Account::makeDeposit(int deposit) {
+    _totalAmount += deposit;
+    
+
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+    
+}
+
+int  Account::checkAmount(void) const {
+    
+}
+
+//Getters
+
+void Account::displayStatus(void) const { 
+
+}
+
 int Account::getNbAccounts( void ) {
     return ( _nbAccounts );
 }
@@ -23,26 +80,10 @@ int	Account::getNbWithdrawals( void ) {
     return ( _totalNbWithdrawals );
 }
 
-void Account::displayAccountsInfos( void ) {
-    std::cout << "accounts:" << _nbAccounts << ";total:" 
-        << _totalAmount << ";deposits:" << _totalNbDeposits 
-        << ";withdrawals:" << _totalNbWithdrawals << "\n";        
-}
 
-
-// Constructor Stub
-Account::Account(int initial_deposit) : _amount(initial_deposit) {
-    // Leave empty for now just to test compilation
-}
-
-// Destructor Stub
+// Destructor
 Account::~Account(void) {
-    // Leave empty
+    
 }
 
-// Other missing methods
-void Account::makeDeposit(int deposit) { (void)deposit; }
-bool Account::makeWithdrawal(int withdrawal) { (void)withdrawal; return false; }
-int  Account::checkAmount(void) const { return _amount; }
-void Account::displayStatus(void) const { }
-void Account::_displayTimestamp(void) { }
+
